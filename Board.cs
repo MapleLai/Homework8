ff060a6c75b0b87b977546f9fff92f72a113c2ab
MapleLai <37803093+MapleLai@users.noreply.github.com>
2018-06-06 00:08:07 +0800

    Add files via upload

---
+++ Board.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.UI;  
+
+public class Board : MonoBehaviour {  
+
+	public Text text;  
+
+	void Start()  
+	{   
+		this.gameObject.GetComponent<Button>().onClick.AddListener(click);  
+	}  
+
+	void click()  
+	{  
+		if (text.gameObject.activeSelf)  
+			StartCoroutine(open());  
+		else  
+			StartCoroutine(close());  
+	}  
+
+	IEnumerator open()  
+	{  
+		float deltaX = 0f;  
+		float deltaY = 100f;  
+		for (int i = 0; i < 10; i++)  
+		{  
+			text.gameObject.SetActive(false);  
+			deltaX =deltaX - 9f;  
+			deltaY =deltaY - 10f;  
+			text.transform.rotation = Quaternion.Euler(deltaX, 0, 0);  
+			text.rectTransform.sizeDelta = new Vector2(text.rectTransform.sizeDelta.x, deltaY);  
+			yield return null;  
+		}  
+	}  
+
+	IEnumerator close()  
+	{  
+		float deltaX = -90f;  
+		float deltaY = 0f;  
+		for (int i = 0; i < 10; i++)  
+		{  
+			text.gameObject.SetActive(true); 
+			deltaX =deltaX + 9f;  
+			deltaY =deltaY + 10f;  
+			text.transform.rotation = Quaternion.Euler(deltaX, 0, 0);  
+			text.rectTransform.sizeDelta = new Vector2(text.rectTransform.sizeDelta.x, deltaY);    
+			yield return null;  
+		}  
+	}  
+		
+}  
